1bff92b4e2ce5842816b9de584ea58b56c4c13c7
Sandra Laguna <sandra@theseedsquad.com>
2021-06-28 21:25:10 +0200

    retoque responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,81 @@
 <template>
-  <div class="flex flex-wrap-reverse m-auto bg-right bg-no-repeat h-auto items-center md:flex-nowrap">
-    <div class="md:w-1/2">
-      <div class="text-black mx-auto w-4/5">
-        <p class="text-3xl font-bold">Computer and laptop shop</p>
-        <p class="text-2xl mb-4">Everything you need</p>
-        <a
-          href="#"
-          class="
-            bg-purple-800
-            py-4
-            px-8
-            text-white
-            font-bold
-            uppercase
-            text-xs
-            rounded
-            hover:bg-gray-200
-            hover:text-gray-800
-          "
-          >BUY NOW</a
-        >
+  <div class="relative w-full h-full flex flex-col">
+    <div class="flex flex-wrap-reverse bg-right bg-no-repeat h-auto items-center md:flex-nowrap">
+      <div class="md:w-1/2">
+        <div class="text-black mx-auto w-4/5">
+          <p class="text-3xl font-bold">Computer and laptop shop</p>
+          <p class="text-2xl mb-4">Everything you need</p>
+          <a
+            href="#"
+            class="
+              bg-purple-800
+              py-4
+              px-8
+              text-white
+              font-bold
+              uppercase
+              text-xs
+              rounded
+              hover:bg-gray-200
+              hover:text-gray-800
+            "
+            >BUY NOW</a
+          >
+        </div>
+      </div>
+
+      <div class="b-intro mb-5 md:w-1/2">
+        <img class="w-4/5 m-auto p-4" src="~assets/img/top_img.png" alt="pc" />
       </div>
     </div>
 
-    <div class="b-intro mb-5 md:w-1/2">
-      <img class="w-4/5 m-auto p-4" src="~assets/img/top_img.png" alt="pc" />
+    <div class="w-full my-12 text-center text-white px-6 py-12 bg-gray-800">
+      <h2 class="text-3xl font-bold">Premium components</h2>
+      <p
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna</p
+      >
+      <img class="m-auto mt-12" src="~assets/img/pcs.png" alt="pc" />
+    </div>
+
+    <div class="w-full text-center px-6">
+      <h2 class="text-3xl font-bold">Build with best</h2>
+      <p
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna</p
+      >
+      <div class="flex flex-col md:flex-row">
+        <div class="bg-green-400 rounded-lg text-white my-6 mx-4 py-6">
+          <h4 class="uppercase text-2xl font-bold"
+            >500GB<br />
+            micro sd card</h4
+          >
+          <p class="p-6 text-xl"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t dolor in reprehenderit in
+            voluptate velit</p
+          >
+        </div>
+        <div class="bg-green-400 rounded-lg text-white my-6 mx-4 py-6">
+          <h4 class="uppercase text-2xl font-bold"
+            >100GB<br />
+            Internal Ram</h4
+          >
+          <p class="p-6 text-xl"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t dolor in reprehenderit in
+            voluptate velit</p
+          >
+        </div>
+        <div class="bg-green-400 rounded-lg text-white my-6 mx-4 py-6">
+          <h4 class="uppercase text-2xl font-bold"
+            >100%<br />
+            High Quality</h4
+          >
+          <p class="p-6 text-xl"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t dolor in reprehenderit in
+            voluptate velit</p
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
